2a1a690a8fe84b8f612ddec836a667d3ee96281c
Dane Harnett <dane_harnett@flightcentre.com>
2013-05-29 15:18:12 +1000

    Adding and/or, changing this to self.

--- condition.js
+++ condition.js
@@ -1,27 +1,52 @@
 (function(window){
   function Condition(options) {
-    var self = this;
-    this.result = true;
-    this.options = options;
-    this.methods = {
+    var self     = this;
+    self.result  = true;
+    self.options = options;
+    self.methods = {
       isDefined: function(value) {
-        return (typeof self.subject !== 'undefined') == value;
+        return (typeof self.subject !== 'undefined') === value;
+      },
+      isFunction: function(value) {
+        return (typeof self.subject === 'function') === value;
+      },
+      isFalse: function(value) {
+        return self.subject === false;
+      },
+      isTrue: function(value) {
+        return self.subject === true;
       }
     };
   }
   // execute the condition, all supplied conditions must pass
   Condition.prototype.exec = function(subject) {
-    var self = this;
-    this.result = true;
-    this.subject = subject;
-    for (var key in this.options){
-      var val = this.options[key];
-      if (typeof self.methods[key] == 'function' && self.methods[key](val) === false) {
+    var self     = this;
+    self.result  = true;
+    self.subject = subject;
+    for (var key in self.options){
+      var val = self.options[key];
+      if (self.execMethod(key, val) === false) {
         self.result = false;
         break;
       }
     }
-    return this;
+    return self;
+  };
+
+  Condition.prototype.execMethod = function(key, value) {
+    var self            = this,
+        returnValue     = true,
+        isFunction      = new Condition({isFunction:true}),
+        isFalse         = new Condition({isFalse:true}),
+        potentialMethod = self.methods[key];
+    
+    isFunction.exec(potentialMethod)
+      .and(isFalse, potentialMethod(value))
+      .then(function(){
+        returnValue = false;
+      });
+    
+    return returnValue;
   };
 
   //
@@ -38,17 +63,39 @@
   Condition.prototype.one = function() {
     // gotta do this
   };
+  Condition.prototype.and = function(conditionObj, subject) {
+    var self = this;
+    if (self.result === true) {
+      // evaluate the "and"
+      return conditionObj.exec(subject);
+    }
+    return self;
+  };
+  Condition.prototype.andAll = function(conditionObj, subjects) {
+  };
+  Condition.prototype.or = function(conditionObj, subject) {
+    var self = this;
+    if (self.result === false) {
+      // evaluate the "or"
+      return conditionObj.exec(subject);
+    }
+    return self;
+  };
+  Condition.prototype.orAll = function(conditionObj, subjects) {
+  };
   Condition.prototype.then = function(func) {
-    if (this.result) {
+    var self = this;
+    if (self.result) {
       func();
     }
-    return this;
+    return self;
   };
   Condition.prototype.otherwise = function(func) {
-    if (!this.result) {
+    var self = this;
+    if (!self.result) {
       func();
     }
-    return this;
+    return self;
   };
   window.Condition = Condition;
 })(window);

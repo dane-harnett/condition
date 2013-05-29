b6f60f11776ad667ec21e05ec7dafa7e209c13dd
Dane Harnett <dane_harnett@flightcentre.com>
2013-05-29 10:27:49 +1000

    Initial possible spec

---
+++ possible-spec.js
@@ -0,0 +1,97 @@
+var condition = new Condition({isDefined:true});
+// or
+var condition = new Condition('do');
+
+var myVar = '';
+condition.exec(myVar)
+  .then(doSomething)
+  .otherwise(doSomethingElse);
+
+
+
+var condition = new Condition({isDefined:true, isObject:true});
+
+// custom wrapper API
+Util.isDefined(myVar).then(func);
+
+
+var myObj = {
+      name:{
+        first:'Dane',
+        last:'Harnett'
+      }
+    };
+var condition = new Condition({
+      isDefined:true,
+      isObject:true,
+      props:[
+      {
+        key:'name',
+        isDefined:true,
+        isOneOf:['a','b'],
+        props:[
+        {
+          key:'first'
+          isDefined:true
+        },
+        {
+          key:'last',
+          isDefined:true
+        }
+      }
+      ]
+    });
+
+
+condition.exec(myObj).then(func);
+
+
+
+
+
+
+var c = new Condition({isDefined:true});
+
+
+c.all(myObj, name, anotherVar).then(func);
+
+c.one(myObj, name).then();
+
+
+var c1 = new Condition({isDefined:true});
+var c2 = new Condition({isDefined:false});
+
+
+c1.exec(myObj)
+  .and(c2, myOtherObj)
+  .then(func);
+
+
+
+c1.exec(myOb)
+  .then(func)
+  .otherwise(function(){
+    c2.exec(otherobj)
+      .then(func);
+  });
+
+condition.eval(myObj) // returns boolean
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
